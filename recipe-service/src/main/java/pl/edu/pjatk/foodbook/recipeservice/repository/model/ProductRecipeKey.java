be73c1b96ae6cd8f5fe60b01681f43695a9b1e0c
Emil Stryszawski <stryszawskiemil@gmail.com>
2023-10-29 13:15:26 +0100

    only parent pom project

--- recipe-service/src/main/java/pl/edu/pjatk/foodbook/recipeservice/repository/model/ProductRecipeKey.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pjatk.foodbook.recipeservice.repository.model;
-
-import java.io.Serializable;
-import java.util.UUID;
-
-public class ProductRecipeKey implements Serializable {
-    private UUID productId;
-    private UUID recipeId;
-}
